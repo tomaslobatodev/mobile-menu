59edc3f69f2dcd057eabf01b9a2c17518331924b
Tomas Lobato <tomaslobatodev@gmail.com>
2023-08-27 09:35:32 -0300

    done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {toggleMenu} from './toggleMenu'
+
+export let menuOpen = false
+
+export const mobilemenuButton = document.querySelector(".mobilemenubtn")
+
+mobilemenuButton.addEventListener("click", toggleMenu)
